ade50f5c7162d6e3981511c5b73ad5ed1edaaa25
ontos67 <art_infinity@inbox.ru>
2023-11-21 10:10:17 +0300

    first commit

---
+++ pkg/api/api.go
@@ -0,0 +1,118 @@
+// API приложения Agrigator.
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	storage "Agrigator/pkg/storage/pstg"
+
+	"github.com/gorilla/mux"
+)
+
+type API struct {
+	db *storage.DB
+	r  *mux.Router
+}
+
+func New(db *storage.DB) *API {
+	a := API{db: db, r: mux.NewRouter()}
+	a.endpoints()
+	return &a
+}
+
+func (api *API) Router() *mux.Router {
+	return api.r
+}
+
+func (api *API) endpoints() {
+	api.r.HandleFunc("/news/last", api.lastArticles).Methods(http.MethodGet, http.MethodOptions)
+	api.r.HandleFunc("/news/lastlist", api.lastArticlesList).Methods(http.MethodGet, http.MethodOptions)
+	api.r.HandleFunc("/news/filter", api.newsFilteredList).Methods(http.MethodGet, http.MethodOptions)
+	api.r.HandleFunc("/news/news", api.newsFullDetailed).Methods(http.MethodGet, http.MethodOptions)
+}
+
+// lastArticles обрабатывает запрос на последние n новостгй с деталями
+// в браузере http://localhost:9998/news/last?n=5
+// n
+func (api *API) lastArticles(w http.ResponseWriter, r *http.Request) {
+	form := r.URL.Query()
+	ns := form.Get("n")
+	n, err := strconv.Atoi(ns)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	news, err := api.db.LastArticles(n)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(news)
+}
+
+// lastArticlesList обрабатывает запрос на последние n новостгй с деталями
+// в браузере http://localhost:9998/news/lastlist?n=5
+// n
+func (api *API) lastArticlesList(w http.ResponseWriter, r *http.Request) {
+	form := r.URL.Query()
+	ns := form.Get("n")
+	n, err := strconv.Atoi(ns)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	news, err := api.db.LastArticlesList(n)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(news)
+}
+
+// newsFilteredDetailed обрабатывает запрос на поиск новости по полям title или
+// в браузере http://localhost:9998/news/filter?time1=1699016144&time2=1700293140&lim=100&field=title&contains=putin&sortfield=id&dir=s
+// time1, time2, Lim, field, content, sortfield, dir
+func (api *API) newsFilteredList(w http.ResponseWriter, r *http.Request) {
+	var fParam storage.FilterParam
+	var err error
+	form := r.URL.Query()
+	fParam.Time[0] = form.Get("time1")
+	fParam.Time[1] = form.Get("time2")
+	fParam.N = form.Get("lim")
+	fParam.Field = form.Get("field")
+	fParam.Contains = "'%" + form.Get("contains") + "%'"
+	fParam.Sort.Field = form.Get("sortfield")
+	dirs := form.Get("dir")
+	fParam.Sort.DirUp = ""
+	if dirs != "" {
+		fParam.Sort.DirUp = "DESC"
+	}
+
+	news, err := api.db.NewsFilteredlist(fParam)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(news)
+}
+
+// NewsFullDetailed обрабатывает запрос на конкретную новость с деталями
+// в браузере http://localhost:9998/news/news?id=5
+// id
+func (api *API) newsFullDetailed(w http.ResponseWriter, r *http.Request) {
+	form := r.URL.Query()
+	ids := form.Get("id")
+	n, err := strconv.Atoi(ids)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	news, err := api.db.NewsFullDetailed(n)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(news)
+}
---
+++ pkg/storage/pstg/storage.go
@@ -0,0 +1,208 @@
+// работа с БД PostgreSQL
+package storage
+
+import (
+	"context"
+	"errors"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+// База данных.
+type DB struct {
+	pool *pgxpool.Pool
+}
+
+// тип сортировки
+type SortParam struct {
+	Field string
+	DirUp string // направление DESC или ""
+}
+
+// параметр фильтрации
+type FilterParam struct {
+	Time     [2]string // период времени
+	N        string    // число в списке
+	Field    string    // поле для поиска
+	Contains string    // содержит...
+	Sort     SortParam // параметры сортировки
+
+}
+
+// Публикация, получаемая из RSS.
+type Article struct {
+	ID        int    // номер записи
+	Title     string // заголовок публикации
+	Content   string // содержание публикации
+	PubTime   int64  // время публикации
+	Url       string // ссылка на источник
+	Publisher string // название источника
+	Autor     string // Имя автора
+}
+
+func New() (*DB, error) {
+	//connstr := os.Getenv("agrigatordb")
+	connstr := "postgres://postgres:" + "plazma" + "@localhost:5432/agrigatordb"
+	if connstr == "" {
+		return nil, errors.New("не указано подключение к БД")
+	}
+	pool, err := pgxpool.Connect(context.Background(), connstr)
+
+	if err != nil {
+		return nil, err
+	}
+	db := DB{
+		pool: pool,
+	}
+	return &db, nil
+}
+
+// SaveArticles Сохраниеть статью в БД
+func (db *DB) SaveArticles(articles []Article) error {
+	for _, article := range articles {
+		_, err := db.pool.Exec(context.Background(), `
+		INSERT INTO articles (title, content, pub_time, a_url, publisher, author)
+		VALUES ($1, $2, $3, $4, $5, $6)
+		ON CONFLICT (title) DO NOTHING`,
+			article.Title,
+			article.Content,
+			article.PubTime,
+			article.Url,
+			article.Publisher,
+			article.Autor,
+		)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// LastArticles возвращает последние N новостей со всеми данными из БД.
+func (db *DB) LastArticles(n int) ([]Article, error) {
+	if n == 0 {
+		n = 10
+	}
+	rows, err := db.pool.Query(context.Background(), `
+	SELECT id, title, content, pub_time, a_url, publisher, author FROM articles
+	ORDER BY pub_time DESC
+	LIMIT $1
+	`,
+		n,
+	)
+	if err != nil {
+		return nil, err
+	}
+	var alist []Article
+	for rows.Next() {
+		var p Article
+		err = rows.Scan(
+			&p.ID,
+			&p.Title,
+			&p.Content,
+			&p.PubTime,
+			&p.Url,
+			&p.Publisher,
+			&p.Autor,
+		)
+		if err != nil {
+			return nil, err
+		}
+		alist = append(alist, p)
+	}
+	return alist, rows.Err()
+}
+
+// LastArticlesList возвращает список последних N тизеров новостей из БД
+// (ID, заголовок и время публикации).
+func (db *DB) LastArticlesList(n int) ([]Article, error) {
+	if n == 0 {
+		n = 9223372036854775807
+	}
+	rows, err := db.pool.Query(context.Background(), `
+	SELECT id, title, pub_time FROM articles
+	ORDER BY pub_time DESC
+	LIMIT $1
+	`,
+		n,
+	)
+	if err != nil {
+		return nil, err
+	}
+	var alist []Article
+	for rows.Next() {
+		var p Article
+		err = rows.Scan(
+			&p.ID,
+			&p.Title,
+			&p.PubTime,
+		)
+		if err != nil {
+			return nil, err
+		}
+		alist = append(alist, p)
+	}
+	return alist, rows.Err()
+}
+
+// NewsFilteredlist возвращает отфильтрованный и отсортированный список новостей
+func (db *DB) NewsFilteredlist(f FilterParam) ([]Article, error) {
+	if f.N == "0" {
+		f.N = "99999"
+	}
+	qstr := "SELECT id, title, pub_time FROM articles " +
+		"WHERE pub_time BETWEEN " + f.Time[0] + " AND " + f.Time[1] +
+		" AND lower(" + f.Field + ") LIKE lower(" + f.Contains + ")" +
+		" ORDER BY " + f.Sort.Field + " " + f.Sort.DirUp +
+		" LIMIT " + f.N
+
+	rows, err := db.pool.Query(context.Background(), qstr)
+	if err != nil {
+		return nil, err
+	}
+	var alist []Article
+	for rows.Next() {
+		var p Article
+		err = rows.Scan(
+			&p.ID,
+			&p.Title,
+			&p.PubTime,
+		)
+		if err != nil {
+			return nil, err
+		}
+		alist = append(alist, p)
+	}
+	return alist, rows.Err()
+}
+
+// NewsFullDetailed возвращает конкретную новость
+func (db *DB) NewsFullDetailed(id int) (Article, error) {
+	var a Article
+	rows, err := db.pool.Query(context.Background(), `
+	SELECT id, title, content, pub_time, a_url, publisher, author FROM articles
+	WHERE id = $1
+	`,
+		id,
+	)
+	if err != nil {
+		return a, err
+	}
+	for rows.Next() {
+		var p Article
+		err = rows.Scan(
+			&p.ID,
+			&p.Title,
+			&p.Content,
+			&p.PubTime,
+			&p.Url,
+			&p.Publisher,
+			&p.Autor,
+		)
+		if err != nil {
+			return a, err
+		}
+		a = p
+	}
+	return a, rows.Err()
+}
